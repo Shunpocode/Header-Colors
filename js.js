ae848b43c24615c095a2e8fe9fa8c2b538518544
Maksym <139382157+Shunpocode@users.noreply.github.com>
2023-12-18 16:52:35 +0300

    change

--- js.js
+++ js.js
@@ -2,17 +2,15 @@ var sectionCount = [];
     for (let i = 0; i < document.getElementsByTagName("section").length; i++){
         sectionCount[i] = document.getElementsByTagName("section")[i];
     }
-var intervalSection = document.createElement('script');
-intervalSection.type = 'text/javascript';
-intervalSection.text ='console.log(sectionCount[1]); setInterval(() => {\nif( "-" + sectionCount[0].clientHeight > sectionCount[0].getBoundingClientRect().y){\n';
+var addJSInterval = document.createElement('script');
+addJSInterval.type = 'text/javascript';
+addJSInterval.text ='console.log(sectionCount[1]); setInterval(() => {\nif( "-" + sectionCount[0].clientHeight > sectionCount[0].getBoundingClientRect().y){\n';
     for (let i = 1, x = 0; i < sectionCount.length; i++) {
-        intervalSection.text = intervalSection.text + '\nif("-" + sectionCount['+ i +'].clientHeight < sectionCount['+ i +'].getBoundingClientRect().y && "-" + sectionCount['+ x +'].clientHeight > sectionCount['+ x +'].getBoundingClientRect().y) \n{\n\tbackColorHeader(sectionCount['+ x +']);\n}';
-        if (x < sectionCount.length.pop()) {
-            x++;
-        }
+        addJSInterval.text = addJSInterval.text + '\nif("-" + sectionCount['+ i +'].clientHeight < sectionCount['+ i +'].getBoundingClientRect().y && "-" + sectionCount['+ x +'].clientHeight > sectionCount['+ x +'].getBoundingClientRect().y) \n{\n\tbackColorHeader(sectionCount['+ x +']);\n}';
+        x++;
     }
-intervalSection.text = intervalSection.text + '\n}else{document.getElementsByTagName("header")[0].style.background = "transparent";}}, 100);';
-document.body.appendChild(intervalSection);
+addJSInterval.text = addJSInterval.text + '\n}else{document.getElementsByTagName("header")[0].style.background = "transparent";}}, 100);';
+document.body.appendChild(addJSInterval);
 
 
 function backColorHeader(element){
